9ebec2b6388e03d585ea7d4dbed1516d9764162d
Rudra Vashishtha <rudraturtle7@gmail.com>
2024-06-09 13:08:56 +0530

    Loading state for Previous Interviews

--- app/dashboard/_components/InterviewList.jsx
+++ app/dashboard/_components/InterviewList.jsx
@@ -9,6 +9,7 @@ import InterviewItemCard from "./InterviewItemCard";
 function InterviewList() {
   const { user } = useUser();
   const [interviewList, setInterviewList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     user && GetInterviewList();
@@ -21,28 +22,33 @@ function InterviewList() {
       .where(eq(schema.createdBy, user?.primaryEmailAddress?.emailAddress))
       .orderBy(desc(schema.id));
 
-    console.log(result);
     setInterviewList(result);
+    setLoading(false);
   };
   return (
     <div>
-        <h2 className='font-medium text-xl'>Previous Interviews</h2>
+      <h2 className="font-medium text-2xl">Previous Interviews</h2>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3'>
-            {interviewList?.length>0?interviewList.map((interview,index)=>(
-                <InterviewItemCard 
-                interview={interview}
-                key={index} />
-            ))
-            :
-            [1,2,3,4].map((item,index)=>(
-                <div className='h-[100px] w-full bg-gray-200 animate-pulse rounded-lg '
-                key={index}
-                >
-                </div>
-            ))
-        }
+      {loading ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3">
+          {[1, 2, 3, 4].map((item, index) => (
+            <div
+              className="h-[100px] w-full bg-gray-200 animate-pulse rounded-lg "
+              key={index}
+            ></div>
+          ))}
         </div>
+      ) : interviewList?.length == 0 ? (
+        <h2 className="font-bold text-xl text-gray-500 text-center mt-20">
+          No Interview Record Found
+        </h2>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3">
+          {interviewList.map((interview, index) => (
+            <InterviewItemCard interview={interview} key={index} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
